97239cd77d7b97d88a1c93aabd59944174c6b48c
Abdul <abdulahad2004@yahoo.com>
2023-02-25 13:35:58 +0300

    OOP implementation

---
+++ src/password.py
@@ -0,0 +1,34 @@
+from random import choice
+from string import ascii_letters
+
+import hashlib
+
+class Password:
+    def __init__(self, password):
+        self.password = password
+        self.salt = self.generate_salt()
+        self.hash = self.create_hash()
+    
+    @staticmethod
+    def generate_salt():
+        """
+        https://docs.python.org/3/library/secrets.html
+        """
+
+        salt = ""
+        length = choice(list(range(10, 21)))
+        indexes = [x for x in range(len(ascii_letters))]
+
+        for i in range(length):
+            index = choice(indexes)
+            salt += ascii_letters[choice(indexes)]
+        
+        return salt
+    
+    def create_hash(self):
+        # Use the python hashlib library to create and return a hash of the user's password
+        pw_hash = hashlib.sha512()
+        pw_hash.update(bytes(self.password + self.salt, encoding="utf-16"))
+        pw_hash = pw_hash.digest()
+
+        return pw_hash
---
+++ src/teacher.py
@@ -0,0 +1,51 @@
+from src.user import User, DB_PATH
+
+from flask import session
+
+import sqlite3
+
+class Teacher(User):
+    def __init__(self, first_name, surname, suffix, email, password):
+        super().__init__(first_name, surname, email, password)
+        self.suffix = suffix
+        self.username = self.create_username()
+    
+    def create_username(self):
+        return self.first_name[0] + "." + self.surname + "_t"
+    
+    def insert_into_db(self):
+        # Method to insert a teacher into the database 
+        con = sqlite3.connect(DB_PATH)
+        cur = con.cursor()
+
+        details = (
+            self.username, 
+            self.first_name,
+            self.surname, 
+            self.suffix,
+            self.email,
+            self.password.hash,
+            self.password.salt
+        )
+        
+        sql = """
+        INSERT INTO
+        teachers
+        (username, first_name, surname, suffix, email, password, salt) 
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """
+
+        cur.execute(sql, details)
+        con.commit()
+        con.close()
+    
+    def save_into_session(self):
+        # Save user details into the session dictionary so that it can be easily accessed by other blueprint modules
+        session["user_info"] = {
+            "username": self.username,
+            "first_name": self.first_name,
+            "surname": self.surname,
+            "suffix": self.suffix,
+            "email": self.email,
+            "is_student": False
+        }
